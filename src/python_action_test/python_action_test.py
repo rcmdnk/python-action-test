6b5a1b7d9d83171b1a16142f54b725361d1697c5
rcmdnk <rcmdnk@gmail.com>
2023-10-07 01:25:36 +0900

    feat: support python 3.9 chore: Update python version requirement in pyproject.toml chore: update pyproject.toml with dev dependency test: update tomlib import test: add import for Path test: macos-latest-xlarge is not available for free (?)

--- src/python_action_test/python_action_test.py
+++ src/python_action_test/python_action_test.py
@@ -2,13 +2,13 @@ import sys
 
 
 def main() -> None:
-    match len(sys.argv):
-        case 1:
-            print("Hello World!")
-        case 2:
-            print(f"Hello {sys.argv[1]}!")
-        case _:
-            print(f"Hello {', '.join(sys.argv[1:])}!")
+    n_arg = len(sys.argv)
+    if n_arg == 1:
+        print("Hello World!")
+    elif n_arg == 2:
+        print(f"Hello {sys.argv[1]}!")
+    else:
+        print(f"Hello {', '.join(sys.argv[1:])}!")
 
 
 if __name__ == "__main__":
--- tests/test_version.py
+++ tests/test_version.py
@@ -1,9 +1,12 @@
-import tomli
-
+from pathlib import Path
 from python_action_test import __version__
 
 
 def test_version():
-    with open("pyproject.toml", "rb") as f:
-        version = tomli.load(f)["tool"]["poetry"]["version"]
+    try:
+        import tomllib
+    except ModuleNotFoundError:
+        import tomli as tomllib
+    with open(Path(__file__).parents[1] / "pyproject.toml", "rb") as f:
+        version = tomllib.load(f)["tool"]["poetry"]["version"]
     assert version == __version__
